150f740752dc91c756a850b5f2bd762a26e72f4d
Shanley <nguyenhieushanley2210@gmail.com>
2025-02-11 19:58:08 +0700

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import routers from '@/routers/routers';
 import { Suspense } from 'react';
 import { SideBarProvider } from '@/contexts/SideBarProvider';
 import SideBar from '@components/SideBar/SideBar';
+import { ToastProvider } from '@/contexts/ToastProvider';
 
 function App() {
     return (
         <>
-            <SideBarProvider>
-                <SideBar />
-                <BrowserRouter>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Routes>
-                            {routers.map((item, index) => {
-                                return (
-                                    <Route
-                                        path={item.path}
-                                        element={<item.component />}
-                                        key={index}
-                                    />
-                                );
-                            })}
-                        </Routes>
-                    </Suspense>
-                </BrowserRouter>
-            </SideBarProvider>
+            <ToastProvider>
+                <SideBarProvider>
+                    <SideBar />
+                    <BrowserRouter>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Routes>
+                                {routers.map((item, index) => {
+                                    return (
+                                        <Route
+                                            path={item.path}
+                                            element={<item.component />}
+                                            key={index}
+                                        />
+                                    );
+                                })}
+                            </Routes>
+                        </Suspense>
+                    </BrowserRouter>
+                </SideBarProvider>
+            </ToastProvider>
         </>
     );
 }
--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -3,11 +3,13 @@ import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { ToastContext } from '@/contexts/ToastProvider';
 
 function Login() {
     const { container, title, boxRememberMe, lostPW } = styles;
     const [isRegister, setIsRegister] = useState(false);
+    const { toast } = useContext(ToastContext);
 
     const formik = useFormik({
         initialValues: {
@@ -78,6 +80,7 @@ function Login() {
                     <Button
                         content={isRegister ? 'REGISTER' : 'LOGIN'}
                         type='submit'
+                        onClick={() => toast.warning('ERR')}
                     />
                 </div>
             </form>
